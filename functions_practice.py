482790fe41b6ecc4efd18b51b687e3b911349466
BrettKendrick <123604258+BrettKendrick@users.noreply.github.com>
2023-12-05 21:15:08 -0800

    assignment

---
+++ functions_practice.py
@@ -0,0 +1,20 @@
+def say_hello():
+  print("hello")
+
+def sum(x,y):
+  return x+y
+
+def average(x,y):
+  return (x+y)/2
+
+def format(first_name, last_name):
+  return f"{last_name}, {first_name}"
+
+def grad_list(first, last, year, student_number):
+  return [first, last, year, student_number]
+
+def over_18(x):
+  return x > 18
+
+def reverse(str):
+  print(str[::-1])
